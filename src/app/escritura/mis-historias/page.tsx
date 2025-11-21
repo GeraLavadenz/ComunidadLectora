5c0198fa29e815387b2167e9ca735e747cd6a36d
Geraldine Lavadenz <riomahabramromamateo@gmail.com>
2025-11-21 18:41:25 -0400

    `Removed Capitulos module and replaced it with Capitulos page, updated MisHistorias page to use new Capitulos page`

--- src/app/escritura/mis-historias/page.tsx
+++ src/app/escritura/mis-historias/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
-import MisHistorias from '../../../modules/escritura/pages/misHistorias';
+import MisHistorias from '../../../modules/escritura/misHistorias';
 
 export default function Page() {
   return (
