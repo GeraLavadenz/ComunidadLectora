8b7396405e95f5b3bfc9c11969afc71eeb2d3b8b
Geraldine Lavadenz <riomahabramromamateo@gmail.com>
2025-11-25 20:54:21 -0400

    `Removed multiple files from the `src/modules` directory`

--- src/modules/biblioteca/utils/filters.ts
+++
@@ -1,6 +0,0 @@
-// =====================================================
-// 🔎 Utilidades de filtrado
-// =====================================================
-export function normalize(s: string) {
-  return s.normalize("NFD").replace(/\p{Diacritic}/gu, "").toLowerCase();
-}
